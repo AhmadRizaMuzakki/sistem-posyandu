ad54b8d6f607a38f45956a8691b93c3757a2081a
Ahmad Riza Muzakki <0110224101@student.nurulfikri.ac.id>
2025-12-21 22:30:45 +0700

    rute login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,18 @@
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
+                        @php
+                            $dashboardRoute = '/';
+                            if (Auth::user()->hasRole('superadmin')) {
+                                $dashboardRoute = route('admin.dashboard');
+                            } elseif (Auth::user()->hasRole('adminPosyandu')) {
+                                $dashboardRoute = route('adminPosyandu.dashboard');
+                            } elseif (Auth::user()->hasRole('orangtua')) {
+                                $dashboardRoute = route('orangtua.dashboard');
+                            }
+                        @endphp
                         <a
-                            href="{{ url('/dashboard') }}"
+                            href="{{ $dashboardRoute }}"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
                         >
                             Dashboard
